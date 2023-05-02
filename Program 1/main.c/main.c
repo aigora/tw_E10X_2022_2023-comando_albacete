ba22c1a63f8730802e736126a22b7eecd0cc68d2
alvarocamposupm <alvaro.campos.coria@alumnos.upm.es>
2023-05-02 18:29:42 +0200

    Creación de vector estructuras
    Creación de vector estructuras y intento de crear algoritmo de lectura y almacenamiento de datos

--- Program 1/main.c/main.c
+++ Program 1/main.c/main.c
@@ -1,10 +1,26 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
+
+typedef struct
+{
+    char nombre[50];
+    char mes[10];
+    float cantidad;
+}tecnologias;
+typedef struct
+{
+    char mes[10];
+    float cantidadtotal;
+}sumatorio;
 
 int main()
 {
     FILE *archivo, *copiat;
+    tecnologias tecnologia[408];//Vector de estructuras con el tamaño justo, 17 tecnologias a 24 meses cada una
+    sumatorio totalmes[12]; //Vector estructuras que dará el total de cada mes
     char contenido[2500];
+    int lectura=0, numerotecnologia=0;
 
     archivo = fopen("generacion_por_tecnologias_21_22.txt", "r");
     //Abre en modo lectura, comprueba que se abre el archivo.
@@ -29,6 +45,8 @@ int main()
     }
     //Si el archivo existe, esta parte del código lee el contenido del mismo y lo
     //imprime en pantalla
+
+
     copiat = fopen("copia.txt", "w");
     //Funcionalidad de copiar el archivo.
     if(copiat == NULL)
@@ -41,5 +59,15 @@ int main()
         fputs(contenido[2500], copiat);
     }
     fclose(copiat);
+    //reabro el archivo.
+    fopen(archivo);
+    //AUN NO SÉ COMO HACER LA LECTURA Y ALMACENAMIENTO ORDENADO DE DATOS.
+//    do
+//    {
+//        lectura = fscanf(archivo, ",")
+//
+//    }while (!feof(archivo));
+
+
     return 0;
 }
