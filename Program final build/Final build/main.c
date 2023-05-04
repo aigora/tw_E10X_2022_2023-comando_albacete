22bfe5b8445ad28a19748a4e4ff7f27ea66c1da3
alvarocamposupm <alvaro.campos.coria@alumnos.upm.es>
2023-05-04 19:19:46 +0200

    Lectura e impresión de fechas
    Lectura e impresión de fechas

--- Program final build/Final build/main.c
+++ Program final build/Final build/main.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include<locale.h>
 
 #define BUFFER_SIZE 6737
 
 int main()
 {
+   setlocale(LC_ALL, "spanish");
    FILE  *archivo;
    char contenido[6737];
-   int contadorlinea=1, numlin; //Variable de contador para el buscador de línea.
-   char linea[500];//Tamaño de linea, sudficiente como para leer una línea entera. La máxima extensión de línea es de 429
+   int contadorlinea=1, numlin, numfecha=0, i; //Variable de contador para el buscador de línea.
+   char linea[500], fechas[25][10];//Tamaño de linea, sudficiente como para leer una línea entera. La máxima extensión de línea es de 429
    char opcion;
+   char *fecha;
    long size;
 
    archivo = fopen("generacion.txt", "r");
@@ -26,7 +29,7 @@ int main()
     fclose(archivo);
     archivo = fopen("generacion.txt", "r");
 
-    printf("Linea o Total?(L) O (T)\n");
+    printf("Linea, Total o Fechas?(L),(T) o (F)\n");
     scanf("%c", &opcion);
     switch(opcion)
     {
@@ -55,10 +58,29 @@ int main()
         else if(numlin>=23)
         printf("Límite de líneas alcanzado\n");
         break;
+    case 'F':
+    case 'f':
+          if (archivo != NULL)
+          {
+            for (i = 0; i < 5; i++)
+                { //Lee hasta la quinta linea;
+                fgets(linea, 500, archivo);
+                fecha = strtok(linea, ",");
+                }
+            while (fecha != NULL && numfecha < 25)
+            {
+                strcpy(fechas[numfecha], fecha);
+                numfecha++;
+                fecha = strtok(NULL, ",");
+            }
+            for (i = 0; i < numfecha; i++) // Imprimir todas las fechas almacenadas
+                printf("%s\n", fechas[i]);
+         }
+         break;
     default:
         printf("Opción no valida");
     }
-
+    fclose(archivo);
    return 0;
 }
 
