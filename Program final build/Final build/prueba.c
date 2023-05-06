0f022a28ecc39cf6648510b4e59c9928b4b762fd
alvarocamposupm <alvaro.campos.coria@alumnos.upm.es>
2023-05-06 18:45:28 +0200

    Nuevas pruebas
    Creación de nuevo archivo de pruebas, conseguí leer la línea de fechas y una línea de energía y que se hiciera el display de manera regulera. Trabajando en actualizaciones

---
+++ Program final build/Final build/prueba.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+int main()
+{
+    FILE *archivo;
+    char linea[500], *token;
+    char fechas[25][20], energias[25][20];
+    int numfecha = 0, numenergia = 0, i;
+    setlocale(LC_ALL,"spanish");
+
+    archivo = fopen("generacion.txt", "r");
+
+    if (archivo != NULL) {
+        // Leer la quinta línea (fecha)
+        for (i = 0; i < 4; i++) {
+            fgets(linea, 500, archivo);
+        }
+        fgets(linea, 500, archivo);
+        token = strtok(linea, ",");
+        while (token != NULL && numfecha < 25) {
+            strcpy(fechas[numfecha], token);
+            numfecha++;
+            token = strtok(NULL, ",");
+        }
+
+        // Leer la sexta línea (energía)
+        fgets(linea, 500, archivo);
+        token = strtok(linea, ",");
+        while (token != NULL && numenergia < 25) {
+            strcpy(energias[numenergia], token);
+            numenergia++;
+            token = strtok(NULL, ",");
+        }
+
+        // Imprimir las fechas y energías
+        for (i = 0; i < numfecha; i++) {
+            printf("%s\t%s\n", fechas[i], energias[i]);
+        }
+
+        fclose(archivo);
+    } else {
+        printf("Error al abrir el archivo.\n");
+    }
+
+    return 0;
+}
+
+
