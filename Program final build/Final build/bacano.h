ec6809dd07faeafcd609ccc8367c8ea14e40ee37
alvarocamposupm <alvaro.campos.coria@alumnos.upm.es>
2023-05-08 02:21:34 +0200

    Creacion de las librerias de funciones

---
+++ Program final build/Final build/bacano.h
@@ -0,0 +1,5 @@
+void leer_fechas(archivo, fechas, &numfecha); //Funcion para leer las fechas del archivo y almacenarlas en el vector fechas
+void imprimir(char vectorito[][20], int num_datos);
+
+
+
