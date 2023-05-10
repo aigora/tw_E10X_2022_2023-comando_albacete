f59ce5e6c4f57b543a2906cb3b1e340f8f203946
alvarocamposupm <alvaro.campos.coria@alumnos.upm.es>
2023-05-10 19:31:48 +0200

    Bastante legal
    Código que consigue leer el archivo almacenar su contenido en un vector de estructuras con la información de las distintas energías y pide al usuario ingresar el nombre de la energía y según ese input saldrá la información de esa energía

--- Programa Alvarito/Alvarito/alvaritomain.c
+++ Programa Alvarito/Alvarito/alvaritomain.c
@@ -61,194 +61,146 @@
 //
 //    return 0;
 //}
+////    for (int i = 5; i < 23; i++) {
+////        printf("%s: ", data[i].name);
+////        for (int j = 0; j < data[i].num_values; j++) {
+////            printf("%f ", data[i].values[j]);
+////        }
+////        printf("\n");
+////    }
 
 
 //#include <stdio.h>
 //#include <stdlib.h>
 //#include <string.h>
+//#include <ctype.h>
+//
 //
 //typedef struct {
-//    int mes;
-//    int anio;
-//} Fecha;
+//    char name[20];
+//    double values[26];
+//    int num_values;
+//} Energias;
 //
 //int main() {
-//    FILE *archivo;
-//    char linea[500];
-//    char *token;
-//    int i, num_fechas = 0;
-//    Fecha fechas[25];
-//
-//    archivo = fopen("generacion.txt", "r");
-//    if (archivo == NULL) {
-//        printf("No se pudo abrir el archivo.");
-//        exit(EXIT_FAILURE);
+//    FILE *file = fopen("generacion.txt", "r");
+//    if (file == NULL) {
+//        printf("Error al abrir el archivo\n");
+//        return 1;
 //    }
 //
-//    // Saltar las primeras cuatro líneas
-//    for (i = 0; i < 4; i++) {
-//        fgets(linea, 500, archivo);
-//    }
+//    Energias data[100];
+//    int num_lines = 0;
 //
-//    // Leer las fechas y almacenarlas en un vector de estructuras Fecha
-//    fgets(linea, 500, archivo);
-//    token = strtok(linea, ",");
-//    while (token != NULL && num_fechas < 25) {
-//        int mes = atoi(token);
-//        token = strtok(NULL, ",");
-//        int anio = atoi(token);
-//        if (mes >= 1 && mes <= 12 && (anio == 2021 || anio == 2022)) {
-//            fechas[num_fechas].mes = mes;
-//            fechas[num_fechas].anio = anio;
-//            num_fechas++;
-//        }
-//        token = strtok(NULL, ",");
-//    }
+//    char line[1000];
+//    while (fgets(line, 1000, file) != NULL) {
+//        // Separa la línea por comas
+//        char *token = strtok(line, ",");
+//        int token_num = 0;
 //
-//    fclose(archivo);
+//        // Guarda el nombre de la energía
+//        strcpy(data[num_lines].name, token);
 //
-//    // Imprimir las fechas almacenadas en el vector de estructuras Fecha
-//    for (i = 0; i < num_fechas; i++) {
-//        printf("Fecha %d: %02d/%04d\n", i+1, fechas[i].mes, fechas[i].anio);
-//    }
-//
-//    return 0;
-//}
-
-//
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
-//
-//#define MAX_ENERGIAS 100
-//
-//typedef struct {
-//    char tipo[50];
-//    float cantidad;
-//} Energia;
-//
-//int main() {
-//    FILE *archivo;
-//    char linea[500];
-//    char *token;
-//    int i, num_energias = 0;
-//    Energia energias[MAX_ENERGIAS];
-//
-//    archivo = fopen("generacion.txt", "r");
-//    if (archivo == NULL) {
-//        printf("No se pudo abrir el archivo.");   //CON ESTO CONSIGO VECTOR PERO NO RELLENARLO.
-//        exit(EXIT_FAILURE);
-//    }
-//
-//    for (i = 0; i < 5; i++) {
-//        fgets(linea, 500, archivo);
-//    }
-//
-//    while (fgets(linea, 500, archivo)) {
-//        token = strtok(linea, ",");
-//        if (token != NULL) {
-//            strcpy(energias[num_energias].tipo, token);
+//        // Guarda los valores como doubles
+//        while (token != NULL) {
 //            token = strtok(NULL, ",");
 //            if (token != NULL) {
-//                energias[num_energias].cantidad = atof(token);
-//                num_energias++;
-//                if (num_energias == MAX_ENERGIAS) {
-//                    printf("Se ha alcanzado el máximo número de energías permitidas.\n");
-//                    break;
-//                }
+//                data[num_lines].values[token_num] = strtod(token + (*token == '"'), NULL);
+//                token_num++;
 //            }
+//        }
+//
+//        // Guarda el número de valores en la estructura
+//        data[num_lines].num_values = token_num;
+//
+//        // Incrementa el contador de líneas
+//        num_lines++;
+//    }
+//
+//    // Cierra el archivo
+//    fclose(file);
+//
+//    char input_name[100];
+//    printf("Ingrese el nombre de la energía que desea buscar: ");
+//    fgets(input_name, 100, stdin);
+//    // Elimina el salto de línea final de la entrada
+//    input_name[strcspn(input_name, "\n")] = '\0';
+//
+//    // Limpiar y normalizar la cadena de entrada
+//    char input_name_clean[100];
+//    char *token = strtok(input_name, " ");
+//    int i = 0;
+//    while (token != NULL) {
+//        for (int j = 0; j < strlen(token); j++) {
+//            if (isalnum(token[j])) {
+//                input_name_clean[i] = tolower(token[j]);
+//                i++;
+//            }
+//        }
+//        token = strtok(NULL, " ");
+//    }
+//    input_name_clean[i] = '\0';
+//
+//    // Busca la coincidencia y muestra los valores correspondientes
+//    int found = 0;
+//    for (int i = 0; i < num_lines; i++) {
+//        // Limpiar y normalizar el nombre almacenado en la estructura
+//        char name_clean[20];
+//        int j = 0;
+//        for (int k = 0; k < strlen(data[i].name); k++) {
+//            if (isalnum(data[i].name[k])) {
+//                name_clean[j] = tolower(data[i].name[k]);
+//                j++;
+//            }
+//        }
+//        name_clean[j] = '\0';
+//
+//        // Comparar las cadenas normalizadas
+//        if (strcmp(name_clean, input_name_clean) == 0) {
+//            found = 1;
+//            printf("%s: ", data[i].name);
+//            for (int j = 0; j < data[i].num_values; j++) {
+//                printf("%f ", data[i].values[j]);
+//            }
+//            printf("\n");
+//            break;
 //        }
 //    }
 //
-//    printf("Se han leído %d energías.\n", num_energias);
-//    printf("Energías leídas:\n");
-//    for (i = 0; i < num_energias; i++) {
-//        printf("%s: %.2f\n", energias[i].tipo, energias[i].cantidad);
+//    if (!found) {
+//        printf("No se encontró la energía ingresada\n");
 //    }
 //
-//    fclose(archivo);
 //    return 0;
 //}
 
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
-//
-//
-//
-//typedef struct {
-//    char tipo[50];
-//    float cantidad;
-//} Energia;
-//
-//int main() {
-//    FILE *archivo;
-//    char linea[500];
-//    int i, num_energias = 0;
-//    Energia energias[5000];
-//
-//    archivo = fopen("generacion.txt", "r");
-//    if (archivo == NULL) {
-//        printf("No se pudo abrir el archivo.");
-//        exit(EXIT_FAILURE);
-//    }
-//
-//    for (i = 0; i < 5; i++) {
-//        fgets(linea, 500, archivo);
-//    }
-//
-//    while (fgets(linea, 500, archivo)) {
-//        if (sscanf(linea, "%[^,],%f", energias[5000].tipo, &energias[5000].cantidad) == 2) {
-//            num_energias++;
-//            if (num_energias == 5000) {
-//                printf("Se ha alcanzado el máximo número de energías permitidas.\n");
-//                break;
-//            }
-//        } else {
-//            printf("Error de formato en la línea: %s", linea);
-//        }
-//    }
-//
-//    printf("Se han leído %d energías.\n", num_energias);
-//    printf("Energías leídas:\n");
-//    for (i = 0; i < num_energias; i++) {
-//        printf("%s: %.2f\n", energias[i].tipo, energias[i].cantidad);
-//    }
-//    fclose(archivo);
-//
-//    return 0;
-//}
-//
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-#define MAX_LINE_LENGTH 1000
-#define MAX_VALUES 26 // Número máximo de valores por línea
 
-// Estructura para guardar los datos de cada línea
-struct EnergyData {
+typedef struct {
     char name[20];
-    double values[MAX_VALUES];
+    double values[26];
     int num_values;
-};
+} Energias;
+
+void convertir_caracteres_especiales(char *cadena);
 
 int main() {
-    // Abre el archivo
     FILE *file = fopen("generacion.txt", "r");
     if (file == NULL) {
         printf("Error al abrir el archivo\n");
         return 1;
     }
 
-    // Crea un vector de estructuras
-    struct EnergyData data[100];
+    Energias data[100];
     int num_lines = 0;
 
-    // Lee el archivo línea por línea
-    char line[MAX_LINE_LENGTH];
-    while (fgets(line, MAX_LINE_LENGTH, file) != NULL) {
+    char line[1000];
+    while (fgets(line, 1000, file) != NULL) {
         // Separa la línea por comas
         char *token = strtok(line, ",");
         int token_num = 0;
@@ -275,14 +227,58 @@ int main() {
     // Cierra el archivo
     fclose(file);
 
-    // Imprime los datos para verificar que se leyeron correctamente
-    for (int i = 5; i < 6; i++) {
-        printf("%s: ", data[i].name);
-        for (int j = 0; j < data[i].num_values; j++) {
-            printf("%f ", data[i].values[j]);
+    char input_name[100];
+    printf("Ingrese el nombre de la energía que desea buscar: ");
+    fgets(input_name, 100, stdin);
+    // Elimina el salto de línea final de la entrada
+    input_name[strcspn(input_name, "\n")] = '\0';
+
+    // Limpiar y normalizar la cadena de entrada
+    char input_name_clean[100];
+    strcpy(input_name_clean, input_name);
+    convertir_caracteres_especiales(input_name_clean);
+    for (int i = 0; i < strlen(input_name_clean); i++) {
+        input_name_clean[i] = tolower(input_name_clean[i]);
+    }
+
+    // Busca la coincidencia y muestra los valores correspondientes
+    int found = 0;
+    for (int i = 0; i < num_lines; i++) {
+        // Limpiar y normalizar el nombre almacenado en la estructura
+        char name_clean[20];
+        strcpy(name_clean, data[i].name);
+        convertir_caracteres_especiales(name_clean);
+        for (int j = 0; j < strlen(name_clean); j++) {
+            name_clean[j] = tolower(name_clean[j]);
         }
-        printf("\n");
+
+        // Comparar las cadenas normalizadas
+        if (strcmp(name_clean, input_name_clean) == 0) {
+            found = 1;
+            printf("%s: ", data[i].name);
+            for (int j = 0; j < data[i].num_values; j++) {
+                printf("%f ", data[i].values[j]);
+            }
+            printf("\n");
+            break;
+        }
+    }
+
+    if (!found) {
+        printf("No se encontró la energía ingresada\n");
     }
 
     return 0;
+}
+
+void convertir_caracteres_especiales(char *cadena) {
+    char origen[] = {'á', 'é', 'í', 'ó', 'ú', 'ñ', 'ü'};
+    char destino[] = {'a', 'e', 'i', 'o', 'u', 'n', 'u'};
+    for (int i = 0; i < strlen(cadena); i++) {
+        for (int j = 0; j < sizeof(origen); j++) {
+            if (cadena[i] == origen[j]) {
+                cadena[i] = destino[j];
+            }
+        }
+    }
 }
